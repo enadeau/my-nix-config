97bc585bf1c1b4ada2e17d52336267faca89ce50
Émile Nadeau <nadeau.emile@gmail.com>
2024-06-13 07:36:20 +0000

    Add rigpgrep and firefox

--- home.nix
+++ home.nix
@@ -22,6 +22,8 @@ in
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
+    pkgs.ripgrep
+    pkgs.firefox
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
