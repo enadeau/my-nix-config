ae5d25d432b3cf390c96601f3da853cab021d41d
Émile Nadeau <nadeau.emile@gmail.com>
2024-06-22 22:08:47 +0000

    Fix: pasting shortcut in kitty

--- terminal.nix
+++ terminal.nix
@@ -2,7 +2,7 @@
   programs.kitty.enable = true;
   programs.kitty.keybindings = {
     "ctrl+c" = "copy_or_interrupt";
-    "ctrl+v" = "past_from_clipboard";
+    "ctrl+v" = "paste_from_clipboard";
     "ctrl+shift+n" = "new_os_window_with_cwd";
   };
   programs.kitty.settings = {
