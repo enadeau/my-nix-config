b1fcee725a896fb5558b970d8b00ea08a9c5ed0f
Émile Nadeau <nadeau.emile@gmail.com>
2024-11-19 22:32:08 +0000

    Swithc from gnome to kde

--- hosts/y4080/configuration.nix
+++ hosts/y4080/configuration.nix
@@ -46,8 +46,12 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = false;
+  services.xserver.desktopManager.gnome.enable = false;
+
+  # Enable the KDE Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
